d76921e349a671914413beef0058e85e4872a408
Francisco <franciscols2011@gmail.com>
2024-10-08 20:56:16 -0300

    Se mejoro un detalle en la velocidad de la transición.

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -129,19 +129,19 @@ const containerVariants = {
 	visible: {
 		opacity: 1,
 		transition: {
-			duration: 0.4,
+			duration: 0.1,
 			ease: "easeInOut",
-			staggerChildren: 0.8,
+			staggerChildren: 0.1,
 		},
 	},
 };
 
 const skillVariants = {
-	hidden: { opacity: 0, scale: 0.8 },
+	hidden: { opacity: 0, scale: 0.2 },
 	visible: {
 		opacity: 1,
 		scale: 1,
-		transition: { duration: 0.4, ease: "easeInOut" },
+		transition: { duration: 0.1, ease: "easeInOut" },
 	},
 };
 
